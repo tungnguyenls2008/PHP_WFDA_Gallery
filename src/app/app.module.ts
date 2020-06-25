9fb6ba03c8f9aed3c5be9f8dfa3cb98e521701bf
OverLord <overlord@localhost.localdomain>
2020-06-25 10:33:25 +0700

    homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ImageGalleryModule} from "./image-gallery/image-gallery.module";
+import {GalleryConfig} from "./image-gallery/token";
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ImageGalleryModule,
   ],
-  providers: [],
+  providers: [
+    {provide: GalleryConfig, useValue: 2}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
